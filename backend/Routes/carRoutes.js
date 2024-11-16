7fc62edaf668ecda09fd7ec92d3dd5f9b043d76b
Shivesh Nandan <kshivesh929@gmail.com>
2024-11-16 11:44:33 +0530

    backend CRUD done

--- backend/Routes/carRoutes.js
+++ backend/Routes/carRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { addcar, getcar } from '../controllers/carControllers.js';
+import { addcar, deletecar, getcar, updatecar } from '../controllers/carControllers.js';
 
 const router = express.Router();
 
 router.post('/addcar', addcar);
+router.put('/:id', updatecar);
+router.delete('/:id', deletecar);
 router.get('/', getcar);
 
 
--- backend/controllers/carControllers.js
+++ backend/controllers/carControllers.js
@@ -17,10 +17,40 @@ export const addcar = async (req, res) => {
   }
 };
 
+export const updatecar = async (req, res) => {
+  const {id} = req.params;
+  const { title, description, tags} = req.body;
+  const found = await car.findById(id);
+  try {
+    if(!found){
+      res.send(404).json({message : "car doesn't found"})
+    }
+    const Updatedcar = await car.findByIdAndUpdate(id,{title,description,tags}
+      
+    );
+    res.status(201).json({ message: 'car added successfully'});
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+export const deletecar = async (req, res) => {
+  const {id} = req.params;
+  const found = await car.findById(id);
+  try {
+    if(!found){
+      res.send(404).json({message : "car doesn't found"})
+    }
+    const deletecar = await car.findByIdAndDelete(id);
+    res.status(201).json({ message: 'car deleted successfully'});
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
 export const getcar = async (req, res) => {
     try {
         const cars = await car.find();
-        console.log("hello")
         if (!cars) {
           return res.status(404).json({ message: 'No car found' });
         }
